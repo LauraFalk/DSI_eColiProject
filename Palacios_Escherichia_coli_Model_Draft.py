628f98d5d22e398d5dcbe1b587c487e13c4d02ff
Laura Palacios <palacila@email.arizona.edu>
2022-10-06 10:48:33 -0700

    Continued to explore modelling options

--- Palacios_Escherichia_coli_Model_Draft.py
+++ Palacios_Escherichia_coli_Model_Draft.py
@@ -75,26 +75,83 @@ from sklearn.feature_selection import RFE
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
 from sklearn.metrics import precision_score, recall_score, roc_curve
-
+## Added by LP
+from sklearn import metrics
 ## Read in the data
-ecoli_csv = pd.read_csv('Data\Processed\ecoli_attributed.csv')
+ec_csv = pd.read_csv('Data\Processed\ecoli_attributed.csv')
 
 #Drop unecessary columns.
-ecoli_csv = ecoli_csv.drop(['Unnamed: 0'], axis=1)
+ec_csv = ec_csv.drop(['Unnamed: 0'], axis=1)
 
 # Create the Array
-ecoli_array = np.array(ecoli_csv)
+ec_array = np.array(ec_csv)
 
 # Create the Array for each criteria (236 and 575)
-ecoli_x = ecoli_array[:, 0:-2]
-ecoli_236 = ecoli_test[:, -2]
-ecoli_575 = ecoli_test[:, -1]
+ec_x = ec_array[:, 0:-2]
+ec_236 = ec_array[:, -2]
+ec_575 = ec_array[:, -1]
 
-#Test to ensure this is working
+# Test to ensure this is working. 
+# Row 16 is the first row which has a different category for 236 and 575 as of 10/6/2022.
+# Prints all rows rather than just the first ones
 pd.set_option('display.max_columns', None)
-print(ecoli_csv.loc[[11]])
-print(ecoli_x[11])
-print(ecoli_236[11])
-print(ecoli_575[11])
+# Print the comparisons
+print(ec_csv.loc[[16]])
+print(ec_x[16])
+print(ec_236[16])
+print(ec_575[16])
 
-# LAURA this isn't working, why??
+# Use the MinMax Scaler to appropriately scale predictor data
+scaler_ec_x = MinMaxScaler()
+ec_x_scl = scaler_ec_x.fit_transform(ec_x)
+
+
+# Split the dataset into test and train. I want to do a 70/30 split.
+x_train_236, x_test_236, y_train_236, y_test_236 = train_test_split(
+  ec_x_scl, ec_236, test_size=0.3, random_state=24)
+
+x_train_575, x_test_575, y_train_575, y_test_575 = train_test_split(
+  ec_x_scl, ec_575_scl, test_size=0.3, random_state=24)
+
+############################################
+
+# Test LR on dataset. Modified from https://towardsdatascience.com
+# Make an instance
+logisticRegr = LogisticRegression()
+
+# Fit the model using all defaults.
+logisticRegr.fit(x_train_236, y_train_236)
+
+# Predict
+predictions = logisticRegr.predict(x_test_236)
+
+# Use score method to get accuracy of model
+score = logisticRegr.score(x_test_236, y_test_236)
+print(score)
+
+###############################
+# Test SVC on dataset. Modified from https://www.datacamp.com
+clf = svm.SVC(kernel='rbf') # Tousi indicates this may be ideal
+
+
+ytrainboo = y_train_236.astype(int)
+#Train the model using the training sets
+clf.fit(x_train_236, y_train_236)
+
+#Predict the response for test dataset
+y_pred = clf.predict(x_test_236)
+
+ytestboo = y_test_236.astype(int)
+# Model Accuracy: how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(x_test_236, ytestboo))
+
+################################
+# Laura - think about Tmin. It is highly correlated with previous30, 
+# Default LR including Tmin is 82.5% success
+# Default LR discluding Tmin is 81.4% success.
+
+# Laura - this is a binary 
+
+# Laura - Your variable name is 235 not 236 - reverify which one I'm supposed to use.
+
+## Laura value error. Classification metrics error. Why is this a continuous multioutput and binary target?
